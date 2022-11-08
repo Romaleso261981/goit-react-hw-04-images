6e2b829748fae6c22a01dbbb2e1fe0724c606c39
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-11-08 18:01:33 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import Notification from './Loader/Loader';
 import Button from './Button/Button';
 import ArticleList from './ImageGallery/ImageGallery';
@@ -8,60 +8,23 @@ import Modal from './Modal/Modal';
 import AppStyled from './AppStyle';
 import Notiflix from 'notiflix';
 
-class App extends Component {
-  state = {
-    isLoading: false,
-    articles: [],
-    query: '',
-    showLargePic: false,
-    showBtn: false,
-    picData: {},
-    page: 1,
-    totalHits: 0,
-    error: false
-  };
+const App = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [articles, setArticles] = useState([]);
+  const [query, setQuery] = useState('');
+  const [showLargePic, setShowLargePic] = useState(false);
+  const [showBtn, setShowBtn] = useState(false);
+  const [picData, setPicData] = useState({});
+  const [page, setPage] = useState(1);
+  const [totalHits, setTotalHits] = useState(0);
+  const [error, setError] = useState(false);
 
-  async componentDidMount() {
-    const { query, page} = this.state;
-    try {
-      const { hits, totalHits } = await fetchImages(query, page);
-      const normalHits = hits.map(
-        ({ id, largeImageURL, webformatURL, tags }) => ({
-          id,
-          largeImageURL,
-          webformatURL,
-          tags,
-        })
-      );
-      this.setState({
-        totalHits: totalHits,
-        articles: [...normalHits],
-        showBtn: page < Math.floor(totalHits / 12),
-        isLoading: false,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  async componentDidUpdate(_, prevState) {
-    const { query, page } = this.state;
-    const { query: prevQuery, page: prevPage } = prevState;
-
-    if (query !== prevQuery || page !== prevPage) {
-      try {
-        this.setState({
-          isLoading: true,
-        });
+  
+  useEffect(() => {
+    (
+      async function () {
+        try {
         const { hits, totalHits } = await fetchImages(query, page);
-        if (!hits.length) {
-          this.setState({
-            isLoading: false,
-            articles: [],
-            error: true
-          });
-          return
-        } 
         const normalHits = hits.map(
           ({ id, largeImageURL, webformatURL, tags }) => ({
             id,
@@ -70,69 +33,102 @@ class App extends Component {
             tags,
           })
         );
-      this.setState({
-        totalHits: totalHits,
-        showBtn: page < Math.floor(totalHits / 12),
-        isLoading: false,
-      });
-        if (query === prevQuery) {
-          this.setState({
-            articles: [...prevState.articles, ...normalHits],
-          });
-        } else {
-          this.setState({
-            articles: [...hits],
-          });
-        }
+        setTotalHits(totalHits)
+        setArticles([...normalHits])
+        setShowBtn(page <Math.floor(totalHits / 12))
+        setIsLoading(false)
       } catch (error) {
         console.log(error);
-      } finally {
-        this.setState({
-          isLoading: false
-        });
-      }
-    }
-  }
+      }}
+    )()
+    
+  }, [page, query])
+ 
+  // async componentDidUpdate(_, prevState) {
+  //   const { query, page } = this.state;
+  //   const { query: prevQuery, page: prevPage } = prevState;
 
-  setQuery = value => {
-    this.setState({ query: value, showBtn: false, page: 1 });
+  //   if (query !== prevQuery || page !== prevPage) {
+  //     try {
+  //       this.setState({
+  //         isLoading: true,
+  //       });
+  //       const { hits, totalHits } = await fetchImages(query, page);
+  //       if (!hits.length) {
+  //         this.setState({
+  //           isLoading: false,
+  //           articles: [],
+  //           error: true
+  //         });
+  //         return
+  //       }
+  //       const normalHits = hits.map(
+  //         ({ id, largeImageURL, webformatURL, tags }) => ({
+  //           id,
+  //           largeImageURL,
+  //           webformatURL,
+  //           tags,
+  //         })
+  //       );
+  //     this.setState({
+  //       totalHits: totalHits,
+  //       showBtn: page < Math.floor(totalHits / 12),
+  //       isLoading: false,
+  //     });
+  //       if (query === prevQuery) {
+  //         this.setState({
+  //           articles: [...prevState.articles, ...normalHits],
+  //         });
+  //       } else {
+  //         this.setState({
+  //           articles: [...hits],
+  //         });
+  //       }
+  //     } catch (error) {
+  //       console.log(error);
+  //     } finally {
+  //       this.setState({
+  //         isLoading: false
+  //       });
+  //     }
+  //   }
+  // }
+
+  const setQuerys = value => {
+    setQuery(value);
+    setShowBtn(false);
+    setPage(1);
   };
 
-  toggleLargeMode = picData => {
-    this.setState(({ showLargePic }) => ({
-      showLargePic: !showLargePic,
-      picData,
-    }));
+  const toggleLargeMode = picData => {
+    setShowLargePic(!showLargePic)
+    setPicData(picData)
   };
 
-  handleLoadMore = () => {
-    this.setState(p => ({ page: p.page + 1 }));
+  const handleLoadMore = () => {
+    setPage(p => ({ page: p.page + 1 }));
   };
 
-  render() {
-    const { articles, showBtn, showLargePic, picData, isLoading, error } = this.state;
-    return (
-      <AppStyled>
-        <SearchBar onSubmit={this.setQuery} />
-        {error && !isLoading && Notiflix.Notify.failure("Нажаль по вашому запиту нічого незнайденно") }
-        {isLoading && <Notification />}
-        <ArticleList
-          articles={articles}
-          toggleLargeMode={this.toggleLargeMode}
+  return (
+    <AppStyled>
+      <SearchBar onSubmit={setQuerys} />
+      {error &&
+        !isLoading &&
+        Notiflix.Notify.failure('Нажаль по вашому запиту нічого незнайденно')}
+      {isLoading && <Notification />}
+      <ArticleList articles={articles} toggleLargeMode={toggleLargeMode} />
+      {showBtn && (
+        <Button
+          onClick={e => {
+            handleLoadMore();
+          }}
         />
-        {showBtn && (
-          <Button
-            onClick={e => {
-              this.handleLoadMore();
-            }}
-          />
-        )}
-        {showLargePic && (
-          <Modal articles={picData} toggleLargeMode={this.toggleLargeMode} />
-        )}
-      </AppStyled>
-    );
-  }
-}
+      )}
+      {showLargePic && (
+        <Modal articles={picData} toggleLargeMode={toggleLargeMode} />
+      )}
+    </AppStyled>
+  );
+};
 
 export default App;
