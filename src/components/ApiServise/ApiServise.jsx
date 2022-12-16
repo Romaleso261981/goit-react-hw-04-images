cfffd5d8206c81562fbb5f4bfc966c596d36ee09
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-12-16 17:27:55 +0300

    Update ApiServise.jsx

--- src/components/ApiServise/ApiServise.jsx
+++ src/components/ApiServise/ApiServise.jsx
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-
 const KEY = '29683186-89d5b8f18ccbe7d45b5194d45';
+// https://pixabay.com/api/?key=29683186-89d5b8f18ccbe7d45b5194d45&image_type=photo&orientation=horizontal&per_page=12
 
 axios.defaults.baseURL = 'https://pixabay.com/api';
 
 const fetchImages = async (query, Page) => {
   const { data } = await axios.get(
-    `/?q=${query}&page=${Page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`,
+    `/?q=${query}&page=${Page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
   );
 
   return data;
